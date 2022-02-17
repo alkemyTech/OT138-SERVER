a3bd9fa89c6c9bb6becb8c30c11008cf63ab5320
Agustín Tornielli <agustint@live.com>
2022-02-17 19:10:21 -0300

    Ot138 32 user registration endpoint (#2)
    * Adding swagger
    
    * Adding Routes and Swagger Config
    
    * Adding Register controller and validation schema
    
    * Fix Migration
    
    * Adding Register Route and Swagger schema
    
    * Adding main routes file
    
    * Adding Swagger
    
    * Adding error prop
    
    * Fix error: true

--- src/app.js
+++ src/app.js
@@ -4,11 +4,26 @@ import cookieParser from "cookie-parser";
 import logger from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
-import db from "./models";
+import routes from "./routes";
+import { ValidationError } from "express-validation";
 dotenv.config();
-
-const app = express();
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUI = require("swagger-ui-express");
+export const app = express();
 const server = http.createServer(app);
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      title: "Alkemy ONG",
+      version: "1.0.0",
+    },
+  },
+  apis: ["src/routes/*.js"],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
 app.use(
   cors({
     credentials: true,
@@ -20,7 +35,15 @@ app.use(logger("dev"));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb" }));
 app.use(cookieParser());
+app.use("/api", routes);
+app.use(function (err, req, res, next) {
+  if (err instanceof ValidationError) {
+    return res.status(err.statusCode).json({ error: true, err });
+  }
 
+  return res.status(500).json({ error: true, err });
+});
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 app.use("/", (_, res) => {
   res.json({ working: true });
 });
