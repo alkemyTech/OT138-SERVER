70f0226e51924208580a7d43a00738361e7f73ae
agustints <agustint@live.com>
2022-02-17 16:33:40 -0300

    Adding error prop

--- src/app.js
+++ src/app.js
@@ -38,10 +38,10 @@ app.use(cookieParser());
 app.use("/api", routes);
 app.use(function (err, req, res, next) {
   if (err instanceof ValidationError) {
-    return res.status(err.statusCode).json(err);
+    return res.status(err.statusCode).json({ error: true, err });
   }
 
-  return res.status(500).json(err);
+  return res.status(500).json({ error: true, err });
 });
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 app.use("/", (_, res) => {
