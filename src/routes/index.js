bb5219f9ffac0d13d23ee9c5342f8e14d24a4022
agustints <agustint@live.com>
2022-02-18 17:28:50 -0300

    Adding /me route with auth middleware protection

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import authRoutes from "./auth.routes";
+import usersRoutes from "./users.routes";
 const router = express.Router();
 
 /* Auth Routes */
 router.use(authRoutes);
+router.use(usersRoutes);
 /* Auth Routes */
 
 export default router;
