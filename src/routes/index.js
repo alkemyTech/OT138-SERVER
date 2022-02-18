a452c12b5e504bae83beba38960f181e8121c63a
Agustín Tornielli <agustint@live.com>
2022-02-18 17:32:00 -0300

    Ot138-36 authenticated user data (#8)
    * Adding me user controller
    
    * Minor fix user middleware
    
    * Adding /me route with auth middleware protection

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import authRoutes from "./auth.routes";
+import usersRoutes from "./users.routes";
 import newsRoutes from "./news.routes";
 const router = express.Router();
 
 /* Auth Routes */
 router.use(authRoutes);
+router.use(usersRoutes);
 /* Auth Routes */
 
 /* News Routes */
