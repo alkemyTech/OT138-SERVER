be0bb11f6793a306e7ae79a4979e1ed84fad5d59
agustints <agustint@live.com>
2022-02-18 17:54:17 -0300

    Fix routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,10 @@
 import express from "express";
 import authRoutes from "./auth.routes";
-import usersRoutes from "./users.routes";
 const router = express.Router();
 
 /* Auth Routes */
 router.use(authRoutes);
-router.use(usersRoutes);
+
 /* Auth Routes */
 
 export default router;
