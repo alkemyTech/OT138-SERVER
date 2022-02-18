160492075f9e20c651b627b362b69b36b24629b9
Facundo Juarez <31292867+facundojuarez2k@users.noreply.github.com>
2022-02-17 23:01:52 -0300

    OT138-40 novedades model migration (#4)
    * Create Entry model and migration
    
    * Add hasMany relation to Category model
    
    * Add unit tests
    
    * Add test command to readme
    
    * Add test dependencies to package.json

--- src/models/category.js
+++ src/models/category.js
@@ -3,22 +3,24 @@
 import { Model } from 'sequelize';
 
 module.exports = (sequelize, DataTypes) => {
-  class Category extends Model {
-    static associate(models) {
-      // define association here
-    }
-  };
-  
-  Category.init({
-    name: DataTypes.STRING,
-    description: DataTypes.STRING,
-    deletedAt: DataTypes.DATE
-  }, {
-    sequelize,
-    modelName: 'Category',
-    paranoid: true
-  });
+    class Category extends Model {
+        static associate(models) {
+            Category.hasMany(models.Entry, {
+                foreignKey: 'categoryId'
+            });
+        }
+    };
 
-  return Category;
+    Category.init({
+        name: DataTypes.STRING,
+        description: DataTypes.STRING,
+        deletedAt: DataTypes.DATE
+    }, {
+        sequelize,
+        modelName: 'Category',
+        paranoid: true
+    });
+
+    return Category;
 
 };
