8581130cf50a577ff3924e3992c193fe0f930471
Facundo Juarez <31292867+facundojuarez2k@users.noreply.github.com>
2022-02-18 18:08:41 -0300

    OT138-56 news update endpoint (#6)
    * Create news update route, controller and validator
    
    * Add update route documentation with swagger

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -34,3 +34,46 @@ export const retrieve = async (req, res) => {
         });
     }
 }
+
+/**
+ * Updates an instance of the Entry given by the 'id' param with the data sent in the request body.
+ */
+ export const update = async (req, res) => {
+    const entryId = req.params.id;
+
+    try {
+        const instance = await Entry.findByPk(entryId);
+
+        if (!instance) {
+            return res.status(200).json({
+                error: true,
+                status: "404",
+                message: "Entry not found",
+            });
+        }
+
+        // Update fields
+        instance.set({
+            ...req.body,
+            deletedAt: instance.deletedAt,
+            createdAt: instance.createdAt,
+            updatedAt: Date.now()
+        })
+
+        await instance.save()
+
+        return res.status(200).json({
+            error: false,
+            status: "200",
+            data: instance,
+        });
+
+    } catch (err) {
+        console.error(err);
+        return res.status(200).json({
+            error: true,
+            status: "500",
+            message: "Internal error",
+        });
+    }
+}
