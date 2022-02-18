91b26508916f300a9f934f3203b8931c5f7aa11e
Agustin-Q <agustin.queija@gmail.com>
2022-02-17 23:20:33 -0300

    Added public data route to router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,14 @@
 import express from "express";
 import authRoutes from "./auth.routes";
+import publicData from "./public.routes";
 const router = express.Router();
 
 /* Auth Routes */
 router.use(authRoutes);
 /* Auth Routes */
+
+/* Public Data Routes */
+router.use(publicData);
+/* Public Data Routes */
 
 export default router;
