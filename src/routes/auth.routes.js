5950755cff4ad154b3b1e9b27b5bb816ecae680f
agustints <agustint@live.com>
2022-02-17 21:34:19 -0300

    Adding Login and refresh tokens routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,7 +1,29 @@
 import express from "express";
 import { validate } from "express-validation";
-import { register, registerValidation } from "../controllers/auth.controller";
+import {
+  login,
+  refresh,
+  register,
+  registerValidation,
+} from "../controllers/auth.controller";
+import { isLoggedIn } from "../middlewares";
 const router = express.Router();
+
+router.post(
+  "/auth/register",
+  validate(registerValidation, {}, {}, {}, {}),
+  register
+);
+
+router.post("/auth/refresh", refresh);
+
+router.post("/auth/login", login);
+
+/* This route protected by middleware is fully illustrative */
+router.get("/auth/protected", isLoggedIn, (req, res) => {
+  res.json({ email: req.email, protected: true });
+});
+
 /**
  * @swagger
  * /api/auth/register:
@@ -27,10 +49,48 @@ const router = express.Router();
  *        description: A successful response
  */
 
-router.post(
-  "/auth/register",
-  validate(registerValidation, {}, {}, {}, {}),
-  register
-);
+/**
+ * @swagger
+ * /api/auth/refresh:
+ *  post:
+ *    description: User Refresh Token
+ *    parameters:
+ *      - in: body
+ *        schema:
+ *          type: object
+ *          required:
+ *             - email
+ *               refreshToken
+ *          properties:
+ *            email:
+ *              type: string
+ *            refreshToken:
+ *              type: string
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ */
+
+/**
+ * @swagger
+ * /api/auth/login:
+ *  post:
+ *    description: User Login Route
+ *    parameters:
+ *      - in: body
+ *        schema:
+ *          type: object
+ *          required:
+ *             - email
+ *               password
+ *          properties:
+ *            email:
+ *              type: string
+ *            password:
+ *              type: string
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ */
 
 export default router;
