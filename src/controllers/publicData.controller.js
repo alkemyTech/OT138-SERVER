1e5797d37d2b90285adb474d430d25b775ed9333
Agustin-Q <agustin.queija@gmail.com>
2022-02-21 00:35:41 -0300

    Change image url, change socialMediaLinks and webLinks to arrays

--- src/controllers/publicData.controller.js
+++ src/controllers/publicData.controller.js
@@ -1,19 +1,21 @@
 const publicData = {
   name: "Aklemy ONG",
-  image: `${process.env.FRONTEND_URL}/public/assets/logo.png`,
+  image: `${process.env.FRONTEND_URL}/logo.png`,
   phone: "+54 11 4876 2158",
   address: "Cabildo 1234, CABA, Buenos Aires, Argentina",
   welcomeText: "Bienvenidos a Alkemy ONG",
-  webLinks: {
-    home: `${process.env.FRONTEND_URL}`,
-    contacts: `${process.env.FRONTEND_URL}/contacts`,
-    faq: `${process.env.FRONTEND_URL}/faq`,
-  },
-  socialMediaLinks: {
-    linkedin: "https://www.linkedin.com/company/alkemy2020/",
-    instagram: "https://www.instagram.com/alkemy__/",
-    facebook: "https://www.facebook.com/AlkemyLATAM/",
-  },
+  webLinks: [
+    { name: "Home", url: `${process.env.FRONTEND_URL}` },
+    { name: "Registro", url: `${process.env.FRONTEND_URL}/registro` },
+    { name: "Login", url: `${process.env.FRONTEND_URL}/login` },
+    { name: "Contacts", url: `${process.env.FRONTEND_URL}/contacts` },
+    { name: "FAQ", url: `${process.env.FRONTEND_URL}/faq` },
+  ],
+  socialMediaLinks: [
+    { name: "Linkedin", url: "https://www.linkedin.com/company/alkemy2020/" },
+    { name: "Instagram", url: "https://www.instagram.com/alkemy__/" },
+    { name: "Facebook", url: "https://www.facebook.com/AlkemyLATAM/" },
+  ],
 };
 
 export const publicDataController = (req, res) => {
