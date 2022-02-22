43d67de380b1459d1f64b3828025ab52111e774d
agustints <agustint@live.com>
2022-02-22 13:17:48 -0300

    Adding Activities Model

---
+++ src/models/activities.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Activities extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Activities.init({
+    name: DataTypes.STRING,
+    image: DataTypes.STRING,
+    content: DataTypes.STRING,
+    deletedAt: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'Activities',
+  });
+  return Activities;
+};
