5457c2931483ec5f6ba554ae6a77ac8b5b8fd0af
Agustin-Q <agustin.queija@gmail.com>
2022-02-21 23:55:11 -0300

    add documentation

--- src/routes/public.routes.js
+++ src/routes/public.routes.js
@@ -5,4 +5,14 @@ const router = express.Router();
 
 router.get("/organizations/1/public", publicDataController);
 
+/**
+ * @swagger
+ * /api/organizations/1/public:
+ *  get:
+ *    description: Public ORganization Data endpoint
+ *    responses:
+ *      '200':
+ *        description: A successful response with public organization data.
+ */
+
 export default router;
