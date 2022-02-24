23543e5d5d0b7bda6ace1ad9915ddade521e7c8c
Agustin-Q <agustin.queija@gmail.com>
2022-02-23 21:51:20 -0300

    Add validation and Database logic

--- src/controllers/activities.controller.js
+++ src/controllers/activities.controller.js
@@ -1,15 +1,41 @@
 import { Joi } from "express-validation";
+import { Activities } from "../models";
 
-const createActivitiesValidation = Joi.object({
-    name: Joi.string()
-      .required(),
-    image: Joi.string()
-      .domain(),
-    content: Joi.string()
-      .required(),
-  }),
+const createActivitiesSchema = Joi.object({
+  name: Joi.string().required(),
+  image: Joi.string().uri(),
+  content: Joi.string().required(),
+});
 export const createActivitiesController = async (req, res) => {
-  const data = await req.json();
-  console.log(data)
-  res.json({success: true});
+  console.log("------activity--------");
+  const { error, value } = createActivitiesSchema.validate(req.body);
+  if (error) {
+    //Validation failed
+    res.json({
+      error: true,
+      status: "400",
+      message: error.message,
+    });
+  } else {
+    console.log(value);
+    //meter datos en la db
+    try {
+      const activity = await Activities.create(value);
+      console.log(activity);
+      res.json({
+        error: false,
+        status: "200",
+        message: "Activity created correctly.",
+      });
+    } catch (error) {
+      console.log(error);
+      res.json({
+        error: true,
+        status: "500",
+        message:
+          "An error occurred while adding activity to the database. Details: " +
+          error.message,
+      });
+    }
+  }
 };
