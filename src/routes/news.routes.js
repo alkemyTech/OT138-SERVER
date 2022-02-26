c1773f9d4bba5e01ae968b4bd7d8f910b776cd3c
Nicolás Oleinizak <nicolas.oleinizak@gmail.com>
2022-02-26 18:08:23 -0300

    add documentation

--- src/routes/news.routes.js
+++ src/routes/news.routes.js
@@ -71,6 +71,31 @@ router.post("/news", updateNewsValidator, isLoggedIn, isAdmin, create);
  *                      application/json:
  *                          schema:
  *                              type: object
+ *      post:
+ *          summary: Create a new entry of type "news"
+ *          parameters:
+ *            - in: body
+ *              schema:
+ *                  type: object
+ *                  required: 
+ *                      - name
+ *                      - content
+ *                  properties:
+ *                      name:
+ *                          type: string
+ *                      categoryId:
+ *                          type: integer
+ *                      image:
+ *                          type: string
+ *                      content:
+ *                          type: string
+ *          responses:
+ *              '200':
+ *                  description: Object with successfully status description
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              type: object
  */
 
 export default router;
