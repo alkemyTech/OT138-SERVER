82f3323ce50db9dc1752315f65618df77ddbbdd3
Facundo Juarez <facu.j.94@gmail.com>
2022-02-28 17:55:20 -0300

    Add update category route

--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -9,7 +9,7 @@ router.post("/categories", createCategoryValidator, create);
 
 /**
  * @swagger
- * /api/category:
+ * /api/categories:
  *      post:
  *        summary: Create a category
  *        parameters:
@@ -26,6 +26,34 @@ router.post("/categories", createCategoryValidator, create);
  *        responses:
  *          '200':
  *            description: The new category
+ *            content:
+ *              application/json:
+ *                schema:
+ *                  type: object
+ * 
+ * /api/categories/{id}: 
+ *      put:
+ *        summary: Update an existing category
+ *        parameters:
+ *          - in: path
+ *            name: id
+ *            schema:
+ *              type: integer
+ *            required: true
+ *            description: Id of the category to update
+ *          - in: body
+ *            schema:
+ *              type: object
+ *              required:
+ *                  - name
+ *              properties:
+ *                name:
+ *                  type: string
+ *                description:
+ *                  type: string
+ *        responses:
+ *          '200':
+ *            description: The updated category
  *            content:
  *              application/json:
  *                schema:
