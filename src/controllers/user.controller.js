6f387d46645b582553e5edc2c23e4bc972485602
Facundo Juarez <facu.j.94@gmail.com>
2022-03-03 16:25:57 -0300

    Update delete controller response format

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -70,24 +70,36 @@ export const list = async (req, res) => {
 export const userDelete = async (req, res) => {
     const id = req.params.id;
     if (!id) {
-        res.status(400).json({
-            message: "ID is not provided",
+        return res.status(200).json({
             error: true,
-            status: "400"
-        })
+            errorCode: 'REQ002',
+            status: "400",
+            message: "User id not provided"
+        });
     } else {
         User.destroy({ where: { id } })
             .then((result) => {
                 if (!result) {
-                    res.status(404).json({ message: "No user", status: "404", error: true });
+                    return res.status(200).json({
+                        error: true,
+                        errorCode: 'REQ001',
+                        status: "404",
+                        message: "User not found"
+                    });
                 } else {
-                    res.status(200).json({ message: "deleted users", result, status: "200", error: false });
+                    return res.status(200).json({
+                        error: false,
+                        status: "204",
+                    });
                 }
             })
             .catch((error) => {
-                res.status(400).json({ message: error, status: "400", error: true });
+                return res.status(200).json({
+                    error: true,
+                    errorCode: 'SRV001',
+                    status: "500",
+                    message: "Internal error"
+                })
             });
-
     }
-
 }
