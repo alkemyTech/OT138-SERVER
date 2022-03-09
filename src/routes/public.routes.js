55515afcb77e4ce874e31805ed8a8737ba2e09c1
Agustin-Q <agustin.queija@gmail.com>
2022-03-09 17:43:46 -0300

    add documentation

--- src/routes/public.routes.js
+++ src/routes/public.routes.js
@@ -23,4 +23,32 @@ router.put(
  *        description: A successful response with public organization data.
  */
 
+/**
+ * @swagger
+ *  /api/organizations/1/public:
+ *  put:
+ *    description: Update Organization Data.
+ *    summary: Update Organization Data.
+ *    consumes:
+ *    - "application/json"
+ *    produces:
+ *    - "application/json"
+ *    parameters:
+ *    - in: body
+ *      description: "Updates Organization Data"
+ *      required: true
+ *      schema:
+ *        type: object
+ *        properties:
+ *          name:
+ *            type: string
+ *            example: "New Organization Name"
+ *          image:
+ *            type: string
+ *            example: "http://placeimg.com/640/480/nature"
+ *            description: "A URL to an image"
+ *    responses:
+ *      '200':
+ *        description: A successful response.
+ */
 export default router;
