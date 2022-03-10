d22469942c0c47015c78c50b702dd57b1a7b5191
Nicolás Oleinizak <nicolas.oleinizak@gmail.com>
2022-03-10 20:08:11 -0300

    Create seeder for members

---
+++ src/seeders/20220310210702-create-members.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const { faker } = require("@faker-js/faker");
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const members = [];
+    for(let i = 0; i < 10; i++){
+      members.push({
+        name: faker.name.firstName()+" "+faker.name.lastName(),
+        image: faker.image.avatar(),
+        createdAt: Date.now(),
+        updatedAt: Date.now()
+      })
+    }
+    console.log("seed")
+    await queryInterface.bulkInsert('Members', members, {})
+  },
+
+  down: async (queryInterface, Sequelize) => {
+     await queryInterface.bulkDelete('People', null, {});
+  }
+};
