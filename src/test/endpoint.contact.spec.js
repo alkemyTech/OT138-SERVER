230c3855afc2e20f2399b6dcff814e9a503f57b6
Agustin-Q <agustin.queija@gmail.com>
2022-03-15 16:17:21 -0300

    add /get routes tests

--- src/test/endpoint.contact.spec.js
+++ src/test/endpoint.contact.spec.js
@@ -3,13 +3,18 @@ const chai = require("chai");
 const chaiHttp = require("chai-http");
 const { async } = require("regenerator-runtime");
 const server = require("../app");
-const { User } = require('../models');
+const { User } = require("../models");
+const {Contacts} = require("../models");
 
 chai.use(chaiHttp);
+chai.use(require('chai-things'));
 
 async function authenticateUser(email, password) {
-  const res = await chai.request(server).post('/api/auth/login').send({ email, password });
-  return res.body.accessToken;
+  const res = await chai
+    .request(server)
+    .post("/api/auth/login")
+    .send({ email, password });
+  return res.body.result.accessToken;
 }
 
 const testData1 = {
@@ -40,15 +45,15 @@ const testData4 = {
 };
 
 
-function standardResponseTest(res, testForError){
+function standardResponseTest(res, testForError) {
   expect(res.status, "Status code should be 200").to.equal(200);
   expect(res.body, "Response body should be an object").to.be.a("object");
-  expect(res.body,'Response body should include the property "error"').to.have.property("error");
-  expect(res.body, "Response body should have the property 'status'" ).to.have.property("status");
-  if (testForError){
+  expect(res.body, 'Response body should include the property "error"').to.have.property("error");
+  expect(res.body, "Response body should have the property 'status'").to.have.property("status");
+  if (testForError) {
     expect(res.body.error, "error should be set to true").to.equal(true);
     expect(res.body, 'Response body should include the property "errorCode"').to.have.property("errorCode");
-  }else {
+  } else {
     expect(res.body.error, "error should be set to false").to.equal(false);
     expect(res.body.status, "Property status should be '200'").to.equal("200");
   }
@@ -62,58 +67,61 @@ describe("Contact endpoint /POST tests:", () => {
   });
 
   it("Should return an error if an extra field is present.", async () => {
-    const res = await chai
-      .request(server)
-      .post("/api/contacts")
-      .send(testData1);
-      standardResponseTest(res, true);
+    const res = await chai.request(server).post("/api/contacts").send(testData1);
+    standardResponseTest(res, true);
     expect(res.body.errorCode, "errorCode should be VAL001").to.equal("VAL001");
   });
 
   it("Should return an error if a required field is missing (name or email).", async () => {
-    const res = await chai
-      .request(server)
-      .post("/api/contacts")
-      .send(testData2);
-      standardResponseTest(res, true);
+    const res = await chai.request(server).post("/api/contacts").send(testData2);
+    standardResponseTest(res, true);
     expect(res.body.errorCode, "errorCode should be VAL001").to.equal("VAL001");
   });
 
   it("Should return an error if a email field is not a valid email.", async () => {
-    const res = await chai
-      .request(server)
-      .post("/api/contacts")
-      .send(testData2);
-      standardResponseTest(res, true);
+    const res = await chai.request(server).post("/api/contacts").send(testData2);
+    standardResponseTest(res, true);
     expect(res.body.errorCode, "errorCode should be VAL001").to.equal("VAL001");
   });
 
   it("Should return a success response if all fields are present.", async () => {
-    const res = await chai
-      .request(server)
-      .post("/api/contacts")
-      .send(testData4);
-      standardResponseTest(res, false);
+    const res = await chai.request(server).post("/api/contacts").send(testData4);
+    standardResponseTest(res, false);
   });
 });
 
 describe("Contact endpoint /get tests:", () => {
-  const EMAIL = "test@email.com"
-  const PASSWORD = "di4id34n7"
+  const EMAIL = "test@email.com";
+  const PASSWORD = "test1234";
+  let accessToken= "";
   before(async () => {
-    await chai.request(server).post('/api/auth/register').send({
-        firstName: 'Test',
-        lastName: 'User',
+      await chai.request(server).post("/api/auth/register").send({
+        firstName: "Test",
+        lastName: "User",
         email: EMAIL,
-        password: PASSWORD
-    });
+        password: PASSWORD,
+      });
+      // Change role to role Id 1 (admin)
+      const userInstance = await User.findOne({ where: { email: EMAIL } });
+      userInstance.roleId = 1;
+      await userInstance.save();
+      // Get Access Token
+      accessToken = await authenticateUser(EMAIL, PASSWORD);
+      //check number of contacts must be >= than 10 for test
+      const numberOfContacts = await Contacts.count();
+      if(numberOfContacts < 10){
+        throw new Error("Number of contacts less than 10, can't perform tests");
+      } else{
+        console.log("Number of contacts: ", numberOfContacts);
+      }
   });
 
-after(async () => {
-  await User.destroy({
-      where: {email: EMAIL},
-      force: true
-    });
+  after(async () => {
+    // Once finished hard delete user form DB
+    await User.destroy({
+        where: {email: EMAIL},
+        force: true
+      });
   });
 
   it("Should return error if no token is present.", async () => {
@@ -123,16 +131,60 @@ after(async () => {
   });
 
   it("Should return error if invalid token is present.", async () => {
-    const res = await chai.request(server).get("/api/contacts").set('Authorization', `Bearer invalidToken`);
-    console.log(res.body);
+    const res = await chai.request(server).get("/api/contacts").set("Authorization", `Bearer invalidToken`);
     standardResponseTest(res, true);
     expect(res.body.errorCode, "errorCode should be AUT001").to.equal("AUT001");
   });
 
-  it("Should return list of contacts.", async () => {
-    const accessToken = await authenticateUser(EMAIL, PASSWORD);
-    const res = await chai.request(server).get("/api/contacts").set('Authorization', `Bearer ${accessToken}`);
-    console.log(res.body);
+  it("Should return an error if page query params is not a number", async () =>{
+    const res = await chai.request(server).get("/api/contacts?page=notANumber").set("Authorization", `Bearer ${accessToken}`);
+    standardResponseTest(res, true);
+    expect(res.body.errorCode, "errorCode should be REQ002").to.equal("REQ002");
+    expect(res.body.status, "status should be 400").to.equal("400");
+  })
+
+  it("Should return an error if limit query params is not a number", async () =>{
+    const res = await chai.request(server).get("/api/contacts?limit=notANumber").set("Authorization", `Bearer ${accessToken}`);
+    standardResponseTest(res, true);
+    expect(res.body.errorCode, "errorCode should be REQ002").to.equal("REQ002");
+    expect(res.body.status, "status should be 400").to.equal("400");
+  })
+
+  it("Should return list of contacts if no query params are present.", async () => {
+    const res = await chai.request(server).get("/api/contacts").set("Authorization", `Bearer ${accessToken}`);
     standardResponseTest(res, false);
+    expect (res.body, "Response should have property 'result'").to.have.property("result");
+    const result = res.body.result;
+    expect (result, "Result should have property 'items'").to.have.property("items");
+    expect (result, "Result should have property 'count'").to.have.property("count");
+    expect (result, "Result should have property 'pages'").to.have.property("pages");
+    expect (result.items, "Items should be an array").to.be.an("array");
+    expect (result.items, "Items should have property id").all.have.property("id");
+    expect (result.items, "Items should have property name").all.have.property("name");
+    expect (result.items, "Items should have property phone").all.have.property("phone");
+    expect (result.items, "Items should have property email").all.have.property("email");
+    expect (result.items, "Items should have property message").all.have.property("message");
   });
+
+  it("Should return list of contacts if query params are present.", async () => {
+    const limit = 3;
+    const page = 2;
+    const res = await chai.request(server).get(`/api/contacts?limit=${limit}&page=${page}`).set("Authorization", `Bearer ${accessToken}`);
+    standardResponseTest(res, false);
+    expect (res.body, "Response should have property 'result'").to.have.property("result");
+    const result = res.body.result;
+    expect (result, "Result should have property 'items'").to.have.property("items");
+    expect (result, "Result should have property 'count'").to.have.property("count",limit);
+    expect (result, "Result should have property 'pages'").to.have.property("pages");
+    expect (result, "Result should have property 'previous'").to.have.property("previous");
+    expect (result, "Result should have property 'next'").to.have.property("next");
+    expect (result.items, "Items should be an array").to.be.an("array");
+    expect (result.items, "Items should have a length equal to limit").to.have.lengthOf(limit);
+    expect (result.items, "Items should have property id").all.have.property("id");
+    expect (result.items, "Items should have property name").all.have.property("name");
+    expect (result.items, "Items should have property phone").all.have.property("phone");
+    expect (result.items, "Items should have property email").all.have.property("email");
+    expect (result.items, "Items should have property message").all.have.property("message");
+  });
+
 });
