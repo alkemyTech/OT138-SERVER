bad3b7c442631d818126999cb964a944dc2e0d0a
Agustin-Q <agustin.queija@gmail.com>
2022-03-15 19:39:16 -0300

    Revert "Ot138 118 test contact endpoint"

--- src/helpers/exceptions.js
+++
@@ -1,7 +0,0 @@
-export class InvalidArgumentsError extends Error {
-    constructor(message, errors) {
-        super(message);
-        this.name = "InvalidArgumentsError";
-        this.errors = errors;
-    }
-}
--- src/routes/sendGrid.routes.js
+++
@@ -1,9 +0,0 @@
-import  express from "express";
-const router = express.Router();
-import {ContactForm,RegistrationForm} from "../controllers/sendGrid.controller"; 
-
-
-router.post("/sendgrid/contact-form",ContactForm);
-router.post("/sendgrid/register-form",RegistrationForm);
-
-export default router;
