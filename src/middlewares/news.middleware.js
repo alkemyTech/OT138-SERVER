ffbb4087f369373b24cd35a5b22741937808a784
Facundo Juarez <facundojuarezf@outlook.com>
2022-03-15 19:10:57 -0300

    Fix news validation middleware

--- src/middlewares/news.middleware.js
+++ src/middlewares/news.middleware.js
@@ -1,24 +1,31 @@
 import { Joi } from 'express-validation';
+import { formatValidationErrors } from '../helpers';
 
 const validationSchema = Joi.object({
     name: Joi.string().max(255).required(),
-    categoryId: Joi.number().integer(),
+    categoryId: Joi.custom((value, helpers) => {
+        if (value === null || (Number.isInteger(value) && value > 0)) {
+            return value;
+        }
+
+        return helpers.message('Field must be a positive integer or null');
+    }),
     type: Joi.string().max(255),
     image: Joi.string().max(255),
     content: Joi.string().required(),
-}).unknown().options({abortEarly: false});
+}).unknown().options({ abortEarly: false });
 
 export const updateNewsValidator = async (req, res, next) => {
 
     const { error, value } = validationSchema.validate(req.body);
 
-    if(error) {
+    if (error) {
         return res.status(200).json({
             error: true,
             status: "400",
             errorCode: "VAL001",
             message: "Invalid data",
-            invalidFields: error.details
+            invalidFields: formatValidationErrors(error)
         });
     }
 
