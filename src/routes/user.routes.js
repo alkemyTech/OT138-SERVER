620db038ec7883fcc628e1864bee0d7896630575
Facundo Juarez <31292867+facundojuarez2k@users.noreply.github.com>
2022-03-18 20:19:37 -0300

    Refactor routes (#78)

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,16 +1,12 @@
 import express from "express";
 const router = express.Router();
-import { list, deleteUser, deleteUserByAdmin, updateProfileByUser, updateProfileByAdmin, getProfileByAdmin } from "../controllers/user.controller";
+import { listUsers, deleteUser, updateUser, getUser } from "../controllers/user.controller";
 import { isLoggedIn, isAdmin } from '../middlewares/auth.middleware';
 
-// Routes for users
-router.put("/users/:id", isLoggedIn, updateProfileByUser);
-router.delete("/users/:id", isLoggedIn, deleteUser);
-
 // Routes for admin
-router.get("/users", isLoggedIn, isAdmin, list);
-router.get("/users/:id", isLoggedIn, isAdmin, getProfileByAdmin);
-router.put("/users/protected/:id", isLoggedIn, isAdmin, updateProfileByAdmin);
-router.delete("/users/protected/:id", isLoggedIn, isAdmin, deleteUserByAdmin);
+router.get("/users", isLoggedIn, isAdmin, listUsers);
+router.get("/users/:id", isLoggedIn, isAdmin, getUser);
+router.put("/users/:id", isLoggedIn, isAdmin, updateUser);
+router.delete("/users/:id", isLoggedIn, isAdmin, deleteUser);
 
 export default router;
