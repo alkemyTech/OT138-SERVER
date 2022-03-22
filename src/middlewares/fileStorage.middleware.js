3eb26ccebed3507ca3eddcb860f6306a3cc3b950
Agustin-Q <agustin.queija@gmail.com>
2022-03-22 11:04:04 -0300

    Implement file storage (#89)
    * Add Multer for file storage
    
    * add uploads folder
    
    * add BACKEND_URL
    
    * Create Middleware to handle file uploads
    
    * add static file serve
    
    * add use of file storage middleware
    
    * Remove console logs
    
    * check for header

---
+++ src/middlewares/fileStorage.middleware.js
@@ -0,0 +1,33 @@
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
+
+function handleFileUpload(fieldName) {
+  return (req, res, next) => {
+    if (req.file) {
+      console.log();
+      if (req.body.json && req.headers["content-type"].toLowerCase().includes("multipart/form-data")) {
+        try {
+          req.body = { ...req.body, ...JSON.parse(req.body.json) };
+          delete req.body.json;
+        } catch (error) {
+          res.json({
+            error: true,
+            errorCode: "REQ001",
+            status: "400",
+            message:
+              "Failed to parse JSON at req.body.json. Details: " +
+              error.message,
+          });
+        }
+      }
+      req.body[fieldName] =
+        process.env.BACKEND_URL + "/" + req.file.path.replace("\\", "/");
+    }
+    next();
+  };
+}
+
+function fileStorageMiddleware(fieldName) {
+  return [upload.single(fieldName), handleFileUpload(fieldName)];
+}
+export default fileStorageMiddleware;
