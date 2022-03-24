3c839cf15659fa260944410339a1c72ca8d6d8f5
Nicolás Oleinizak <78864158+nicolasoleinizak@users.noreply.github.com>
2022-03-24 00:13:50 -0300

    Add file storage middleware, enhance code style, update variable names (#95)

--- src/routes/slider.routes.js
+++ src/routes/slider.routes.js
@@ -1,11 +1,12 @@
 import express from "express";
 const router = express.Router();
 import {SliderGet,SliderPut,SliderDelete,SliderPost} from "../controllers/slides.controller";
+import fileStorageMiddleware from "../middlewares/fileStorage.middleware";
 
 
 router.get("/slides",SliderGet);
-router.post("/slides",SliderPost);
-router.put("/slides/:id",SliderPut);
+router.post("/slides", fileStorageMiddleware("image"), SliderPost);
+router.put("/slides/:id", fileStorageMiddleware("image"), SliderPut);
 router.delete("/slides/:id",SliderDelete);
 
 export default router;
