8ef3a78fe08912e03c29c9ec16491fcfd9caf940
Facundo Juarez <facundojuarezf@outlook.com>
2022-03-24 16:28:47 -0300

    Update members seeder

--- src/seeders/20220310210702-create-members.js
+++ src/seeders/20220310210702-create-members.js
@@ -1,19 +1,83 @@
 "use strict";
 
 const { faker } = require("@faker-js/faker");
+faker.setLocale("es");
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const members = [];
-    for (let i = 0; i < 10; i++) {
-      members.push({
-        name: faker.name.firstName() + " " + faker.name.lastName(),
-        image: faker.image.avatar(),
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      });
-    }
-    console.log("seed");
+    const date = new Date();
+    const members = [
+      {
+        name: 'Andrea Becerra',
+        image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/712.jpg',
+        area: 'Tesorera',
+        createdAt: date,
+        updatedAt: date
+      },
+      {
+        name: 'Rubén Ramírez',
+        image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/87.jpg',
+        area: 'Presidente',
+        createdAt: date,
+        updatedAt: date
+      },
+      {
+        name: 'Laura Salazar',
+        image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/544.jpg',
+        area: 'Vice presidente',
+        createdAt: date,
+        updatedAt: date
+      },
+      {
+        name: 'Ángela Pineda',
+        image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/861.jpg',
+        area: 'Relaciones públicas',
+        createdAt: date,
+        updatedAt: date
+      },
+      {
+        name: 'Lorena García',
+        image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/751.jpg',
+        area: 'Coordinadora',
+        createdAt: date,
+        updatedAt: date
+      },
+      {
+        name: 'Salvador Suárez',
+        image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1118.jpg',
+        area: 'Dirección',
+        createdAt: date,
+        updatedAt: date
+      },
+      {
+        name: 'Pablo Gonzalez',
+        image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/734.jpg',
+        area: 'Vocal',
+        createdAt: date,
+        updatedAt: date
+      },
+      {
+        name: 'Jorge Dueñas',
+        image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/847.jpg',
+        area: 'Secretario',
+        createdAt: date,
+        updatedAt: date
+      },
+      {
+        name: 'Pedro Sánchez',
+        image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/994.jpg',
+        area: 'Vocal',
+        createdAt: date,
+        updatedAt: date
+      },
+      {
+        name: 'Daniel Rivas',
+        image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1043.jpg',
+        area: 'Consultor técnico',
+        createdAt: date,
+        updatedAt: date
+      }
+    ];
     await queryInterface.bulkInsert("Members", members, {});
   },
 
