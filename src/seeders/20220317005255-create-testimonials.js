aa2ed5781bbcfce24a44da91f7055a27b87b69d2
Facundo Juarez <31292867+facundojuarez2k@users.noreply.github.com>
2022-03-24 17:17:08 -0300

    Update testimonial seeder (#97)

--- src/seeders/20220317005255-create-testimonials.js
+++ src/seeders/20220317005255-create-testimonials.js
@@ -1,15 +1,18 @@
 "use strict";
 const { faker } = require("@faker-js/faker");
+faker.setLocale("es");
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    const date = new Date();
     const testimonials = [];
     for (let i = 0; i < 60; i++) {
       testimonials.push({
-        name: faker.lorem.sentence(),
-        image: faker.image.imageUrl(1024, 768, "arch", true),
+        name: faker.name.firstName() + " " + faker.name.lastName(),
+        image: faker.image.avatar(),
         content: faker.lorem.sentence(),
-        createdAt: new Date(),
-        updatedAt: new Date(),
+        createdAt: date,
+        updatedAt: date
       });
     }
     await queryInterface.bulkInsert("Testimonials", testimonials, {});
