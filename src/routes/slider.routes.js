6d6893e2b4e3501158588c7b9b90cb1f879c65e6
Facundo Juarez <facundojuarezf@outlook.com>
2022-03-26 17:25:53 -0300

    Add admin middleware to slides routes

--- src/routes/slider.routes.js
+++ src/routes/slider.routes.js
@@ -1,12 +1,13 @@
 import express from "express";
 const router = express.Router();
-import {SliderGet,SliderPut,SliderDelete,SliderPost} from "../controllers/slides.controller";
+import { isLoggedIn, isAdmin } from '../middlewares/auth.middleware';
+import { SliderGet, SliderPut, SliderDelete, SliderPost} from "../controllers/slides.controller";
 import fileStorageMiddleware from "../middlewares/fileStorage.middleware";
 
 
 router.get("/slides",SliderGet);
-router.post("/slides", fileStorageMiddleware("image"), SliderPost);
-router.put("/slides/:id", fileStorageMiddleware("image"), SliderPut);
-router.delete("/slides/:id",SliderDelete);
+router.post("/slides", isLoggedIn, isAdmin, fileStorageMiddleware("image"), SliderPost);
+router.put("/slides/:id", isLoggedIn, isAdmin, fileStorageMiddleware("image"), SliderPut);
+router.delete("/slides/:id", isLoggedIn, isAdmin, SliderDelete);
 
 export default router;
