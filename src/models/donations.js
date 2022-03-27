ca01bee64bbd991ca65041277dd6b3fd72522bee
Facundo Juarez <31292867+facundojuarez2k@users.noreply.github.com>
2022-03-27 19:51:40 -0300

    Add donation model association (#101)

--- src/models/donations.js
+++ src/models/donations.js
@@ -8,7 +8,10 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Donations.belongsToMany(models.User, {
+        through: models.UserDonations,
+        foreignKey: 'id_donation'
+      });
     }
   }
   Donations.init(
